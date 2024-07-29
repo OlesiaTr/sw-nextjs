2d01b95119809b14925f651b59b82c86301be3f3
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-07-29 13:17:48 +0300

    + use-infinite-scroll hook

---
+++ src/hooks/use-infinite-scroll.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+
+interface UseInfiniteScrollProps {
+  loading: boolean
+  currentPage: number
+  loadMore: () => void
+}
+
+export const useInfiniteScroll = ({
+  loading,
+  currentPage,
+  loadMore,
+}: UseInfiniteScrollProps) => {
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop >=
+          document.documentElement.offsetHeight - 100 &&
+        !loading
+      ) {
+        if (currentPage) {
+          loadMore()
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [currentPage, loading, loadMore])
+}
